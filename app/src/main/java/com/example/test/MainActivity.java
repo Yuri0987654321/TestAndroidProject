b657d2a1212086891a782abe1d40b09f6f196ec7
Yuri <l_yuri@mail.ru>
2019-01-06 13:45:45 +0300

    Удалил Activity2

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_open_activity_2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Activity2.class);
-                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Not implemented", Toast.LENGTH_SHORT)
+                        .show();
             }
         });
 
